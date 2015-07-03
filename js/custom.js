9e79b954b7d8b96ba300f777ffd2caca6efa2ab2
marklnz <markl.nz@hotmail.com>
2015-07-03 18:15:59 +1200

    Initial checkin

---
+++ js/custom.js
@@ -0,0 +1,160 @@
+// Custom Script
+// Developed by: Samson.Onna
+var customScripts = {
+    profile: function () {
+        // portfolio
+        if ($('.isotopeWrapper').length) {
+            var $container = $('.isotopeWrapper');
+            var $resize = $('.isotopeWrapper').attr('id');
+            // initialize isotope
+            $container.isotope({
+                itemSelector: '.isotopeItem',
+                resizable: false, // disable normal resizing
+                masonry: {
+                    columnWidth: $container.width() / $resize
+                }
+            });
+            $("a[href='#top']").click(function () {
+                $("html, body").animate({ scrollTop: 0 }, "slow");
+                return false;
+            });
+            $('.navbar-inverse').on('click', 'li a', function () {
+                $('.navbar-inverse .in').addClass('collapse').removeClass('in').css('height', '1px');
+            });
+            $('#filter a').click(function () {
+                $('#filter a').removeClass('current');
+                $(this).addClass('current');
+                var selector = $(this).attr('data-filter');
+                $container.isotope({
+                    filter: selector,
+                    animationOptions: {
+                        duration: 1000,
+                        easing: 'easeOutQuart',
+                        queue: false
+                    }
+                });
+                return false;
+            });
+            $(window).smartresize(function () {
+                $container.isotope({
+                    // update columnWidth to a percentage of container width
+                    masonry: {
+                        columnWidth: $container.width() / $resize
+                    }
+                });
+            });
+        }
+    },
+    fancybox: function () {
+        // fancybox
+        $(".fancybox").fancybox();
+    },
+    onePageNav: function () {
+
+        $('#mainNav').onePageNav({
+            currentClass: 'active',
+            changeHash: false,
+            scrollSpeed: 950,
+            scrollThreshold: 0.2,
+            filter: '',
+            easing: 'swing',
+            begin: function () {
+                //I get fired when the animation is starting
+            },
+            end: function () {
+                //I get fired when the animation is ending
+            },
+            scrollChange: function ($currentListItem) {
+                //I get fired when you enter a section and I pass the list item of the section
+            }
+        });
+    },
+    slider: function () {
+        $('#da-slider').cslider({
+            autoplay: true,
+            bgincrement: 0
+        });
+    },
+    owlSlider: function () {
+        var owl = $("#owl-demo");
+        owl.owlCarousel();
+        // Custom Navigation Events
+        $(".next").click(function () {
+            owl.trigger('owl.next');
+        })
+        $(".prev").click(function () {
+            owl.trigger('owl.prev');
+        })
+    },
+    bannerHeight: function () {
+        var bHeight = $(".banner-container").height();
+        $('#da-slider').height(bHeight);
+        $(window).resize(function () {
+            var bHeight = $(".banner-container").height();
+            $('#da-slider').height(bHeight);
+        });
+    },
+    init: function () {
+        customScripts.onePageNav();
+        customScripts.profile();
+        customScripts.fancybox();
+        customScripts.slider();
+        customScripts.owlSlider();
+        customScripts.bannerHeight();
+    }
+}
+
+$(function() {
+    // Get the form.
+    var form = $('#contactfrm');
+
+    // Get the messages div.
+    var formMessages = $('#contact');
+
+    // Set up an event listener for the contact form.
+	$(form).submit(function(event) {
+		// Stop the browser from submitting the form.
+		event.preventDefault();
+
+		// TODO
+	});
+
+	// Serialize the form data.
+	var formData = $(form).serialize();	
+	
+	// Submit the form using AJAX.
+	$.ajax({
+		type: 'POST',
+		url: $(form).attr('action'),
+		data: formData
+	})
+	.done(function(response) {
+		// Make sure that the formMessages div has the 'success' class.
+		$(formMessages).removeClass('error');
+		$(formMessages).addClass('success');
+
+		// Set the message text.
+		$(formMessages).text(response);
+
+		// // Clear the form.
+		// $('#name').val('');
+		// $('#email').val('');
+		// $('#message').val('');
+	})
+	.fail(function(data) {
+		// Make sure that the formMessages div has the 'error' class.
+		$(formMessages).removeClass('success');
+		$(formMessages).addClass('error');
+
+		// Set the message text.
+		if (data.responseText !== '') {
+			$(formMessages).text(data.responseText);
+		} else {
+			$(formMessages).text('Oops! An error occured and your message could not be sent.');
+		}
+	});
+});
+
+$('document').ready(function () {
+    customScripts.init();
+});
