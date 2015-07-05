1622ab7b3204098b161ee22a5db2b386202ff056
marklnz <markl.nz@hotmail.com>
2015-07-05 13:12:11 +1200

    Diagnosing pp

--- mailer.php
+++ mailer.php
@@ -26,16 +26,17 @@
         $email_content .= "Email: $email\n\n";
         $email_content .= "Message:\n$message\n";
 		
-        // Send the email.
-		if (smtp_mail($recipient, $email, $email_content)) {
-            // Set a 200 (okay) response code.
-            http_response_code(200);
-            return "Thank You! Your message has been sent.";
-        } else {
-            // Set a 500 (internal server error) response code.
-            http_response_code(500);
-            return $result;
-        }
+        // // Send the email.
+		// if (smtp_mail($recipient, $email, $email_content)) {
+            // // Set a 200 (okay) response code.
+            // http_response_code(200);
+            // return "Thank You! Your message has been sent.";
+        // } else {
+            // // Set a 500 (internal server error) response code.
+            // http_response_code(500);
+            // return $result;
+        // }
+		return $email_content
 
     } else {
         // Not a POST request, set a 403 (forbidden) response code. 
