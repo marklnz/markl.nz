0b9b103c1888ee21032a2c8d14e90d6e7cdd50b0
marklnz <markl.nz@hotmail.com>
2015-07-06 18:17:25 +1200

    Trying to get the emailer working again

--- mailer.php
+++ mailer.php
@@ -1,5 +1,5 @@
 <?php
-	
+		
 	// Only process POST requests.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Get the form fields and remove whitespace.
@@ -33,11 +33,11 @@
 		if (smtp_mail($recipient, $email, $email_content)) {
             // Set a 200 (okay) response code.
             http_response_code(200);
-            echo "Thank You! Your message has been sent.";
+            return "Thank You! Your message has been sent.";
         } else {
             // Set a 500 (internal server error) response code.
             http_response_code(500);
-            echo $result;
+            return $result;
         }
     } else {
         // Not a POST request, set a 403 (forbidden) response code. 
